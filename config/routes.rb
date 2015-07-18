73ea0c1f73961dbc3f4d5d4620c70a03524774c3
Nienke <nienkedekker@gmail.com>
2015-07-18 19:49:20 +0200

    Some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 
 Rails.application.routes.draw do
+
+  get 'articles/index'
+
   resources :articles
+
+  root 'articles#index'
+
+  resources :feeds
 
   get 'refresh' => 'articles#refresh'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :feeds
 end
